15ef29942bd3b78199f1dc23a0d4641b6fa885e5
adalbert <>
2018-08-21 12:51:56 +0000

    init list view and its adapter

--- app/src/main/java/de/radioshuttle/mqttpushclient/MessagesActivity.java
+++ app/src/main/java/de/radioshuttle/mqttpushclient/MessagesActivity.java
@@ -6,10 +6,16 @@
 
 package de.radioshuttle.mqttpushclient;
 
-import android.arch.lifecycle.ViewModel;
+import android.arch.lifecycle.Observer;
+import android.arch.lifecycle.ViewModelProviders;
+import android.arch.paging.PagedList;
 import android.content.Intent;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.DividerItemDecoration;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuInflater;
@@ -20,14 +26,12 @@ import android.widget.TextView;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import de.radioshuttle.db.MqttMessage;
+
 import static de.radioshuttle.mqttpushclient.AccountListActivity.RC_SUBSCRIPTIONS;
 import static de.radioshuttle.mqttpushclient.EditAccountActivity.PARAM_ACCOUNT_JSON;
 
 public class MessagesActivity extends AppCompatActivity {
-
-    static class MessagesViewModel extends ViewModel {
-        public PushAccount pushAccount;
-    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -38,6 +42,7 @@ public class MessagesActivity extends AppCompatActivity {
         Bundle args = getIntent().getExtras();
         String json = args.getString(PARAM_ACCOUNT_JSON);
         boolean hastMultipleServer = args.getBoolean(PARAM_MULTIPLE_PUSHSERVERS);
+        mListView = findViewById(R.id.messagesListView);
         try {
             PushAccount b = PushAccount.createAccountFormJSON(new JSONObject(json));
             TextView server = findViewById(R.id.push_notification_server);
@@ -47,9 +52,30 @@ public class MessagesActivity extends AppCompatActivity {
             if (!hastMultipleServer) {
                 server.setVisibility(View.GONE);
             }
+            mViewModel = ViewModelProviders.of(
+                    this, new MessagesViewModel.Factory(b.pushserverID, b.getMqttAccountName(), getApplication()))
+                    .get(MessagesViewModel.class);
+            if (mViewModel.pushAccount == null)
+                mViewModel.pushAccount = b;
+
+            final MessagesPagedListAdapter adapter = new MessagesPagedListAdapter(this);
+            mListView.setAdapter(adapter);
+            mViewModel.messagesPagedList.observe(this, new Observer<PagedList<MqttMessage>>() {
+                @Override
+                public void onChanged(@Nullable PagedList<MqttMessage> mqttMessages) {
+                    adapter.submitList(mqttMessages);
+                }
+            });
+
         } catch (JSONException e) {
             Log.e(TAG, "parse error", e);
         }
+
+        RecyclerView.ItemDecoration itemDecoration =
+                new DividerItemDecoration(this, DividerItemDecoration.VERTICAL);
+        mListView.addItemDecoration(itemDecoration);
+        mListView.setItemAnimator(null);
+        mListView.setLayoutManager(new LinearLayoutManager(this));
 
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
     }
@@ -77,11 +103,21 @@ public class MessagesActivity extends AppCompatActivity {
                     startActivityForResult(intent, RC_SUBSCRIPTIONS);
                 }
                 return true;
-
+            case R.id.menu_refresh :
+                doRefresh();
+                return true;
             default:
                 return super.onOptionsItemSelected(item);
         }
     }
+
+    protected void doRefresh() {
+        if (mListView != null && mListView.getAdapter() != null) {
+
+            ((MessagesPagedListAdapter) mListView.getAdapter()).refresh();
+        }
+    }
+
 
     @Override
     public void onBackPressed() {
@@ -102,6 +138,8 @@ public class MessagesActivity extends AppCompatActivity {
 
     public final static String PARAM_MULTIPLE_PUSHSERVERS = "PARAM_MULTIPLE_PUSHSERVERS";
 
+    private RecyclerView mListView;
+    private MessagesViewModel mViewModel;
     private boolean mActivityStarted;
 
     private final static String TAG = MessagesActivity.class.getSimpleName();
