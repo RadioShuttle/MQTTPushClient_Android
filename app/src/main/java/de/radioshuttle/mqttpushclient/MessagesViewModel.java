57a0441d7653f10d12bb4ada849745a0c51fc25a
adalbert <>
2018-08-21 14:56:01 +0000

    added refresh() method which just invalidates the data source. Added broadcast receiver which listens for db updates (=incoming messages)

--- app/src/main/java/de/radioshuttle/mqttpushclient/MessagesViewModel.java
+++ app/src/main/java/de/radioshuttle/mqttpushclient/MessagesViewModel.java
@@ -7,20 +7,53 @@
 package de.radioshuttle.mqttpushclient;
 
 import android.app.Application;
+import android.arch.lifecycle.AndroidViewModel;
 import android.arch.lifecycle.LiveData;
+import android.arch.lifecycle.MutableLiveData;
 import android.arch.lifecycle.ViewModel;
 import android.arch.lifecycle.ViewModelProvider;
+import android.arch.paging.DataSource;
 import android.arch.paging.LivePagedListBuilder;
 import android.arch.paging.PagedList;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
 import android.support.annotation.NonNull;
+import android.support.v4.content.LocalBroadcastManager;
 
 import de.radioshuttle.db.AppDatabase;
 import de.radioshuttle.db.MqttMessage;
 import de.radioshuttle.db.MqttMessageDao;
 
-public class MessagesViewModel extends ViewModel {
+public class MessagesViewModel extends AndroidViewModel {
     public LiveData<PagedList<MqttMessage>> messagesPagedList;
     public PushAccount pushAccount;
+
+    public MessagesViewModel(String pushServer, String account, Application app) {
+        super(app);
+        MqttMessageDao dao = AppDatabase.getInstance(app).mqttMessageDao();
+        messagesPagedList = new LivePagedListBuilder<>(
+                dao.getReceivedMessages(pushServer, account), 20).build(); //TODO: page size
+        IntentFilter intentFilter = new IntentFilter(MqttMessage.UPDATE_INTENT);
+        LocalBroadcastManager.getInstance(app).registerReceiver(broadcastReceiver, intentFilter);
+
+    }
+
+    public void refresh() {
+        if (messagesPagedList != null && messagesPagedList.getValue() != null) {
+            DataSource ds = messagesPagedList.getValue().getDataSource();
+            if (ds != null) {
+                ds.invalidate();
+            }
+        }
+    }
+
+    @Override
+    protected void onCleared() {
+        super.onCleared();
+        LocalBroadcastManager.getInstance(getApplication()).unregisterReceiver(broadcastReceiver);
+    }
 
     public static class Factory implements ViewModelProvider.Factory {
 
@@ -33,18 +66,22 @@ public class MessagesViewModel extends ViewModel {
         @NonNull
         @Override
         public <T extends ViewModel> T create(@NonNull Class<T> modelClass) {
-            MessagesViewModel mv = new MessagesViewModel();
-            final MqttMessageDao dao = AppDatabase.getInstance(app).mqttMessageDao();
-
-            mv.messagesPagedList = new LivePagedListBuilder<>(
-                    dao.getReceivedMessages(pushServer, account), 20).build(); //TODO: page size
-
-            return (T) mv;
+            return (T) new MessagesViewModel(pushServer, account, app);
         }
 
         String pushServer;
         String account;
         Application app;
     }
+
+    BroadcastReceiver broadcastReceiver = new BroadcastReceiver() {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            String arg = intent.getStringExtra(MqttMessage.ARG_ACCOUNT);
+            if (arg != null && pushAccount != null && pushAccount.getMqttAccountName().equals(arg)) {
+                refresh();
+            }
+        }
+    };
 
 }
