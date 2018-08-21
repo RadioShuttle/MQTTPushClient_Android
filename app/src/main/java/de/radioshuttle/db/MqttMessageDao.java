5ea14ce75ba7416375e9685364627f19ced281c6
adalbert <>
2018-08-21 12:59:46 +0000

    added some queries. getReceivedMessages() now returns a datasource factory, for usage in paging framework

--- app/src/main/java/de/radioshuttle/db/MqttMessageDao.java
+++ app/src/main/java/de/radioshuttle/db/MqttMessageDao.java
@@ -6,6 +6,7 @@
 
 package de.radioshuttle.db;
 
+import android.arch.paging.DataSource;
 import android.arch.persistence.room.Dao;
 import android.arch.persistence.room.Insert;
 import android.arch.persistence.room.Query;
@@ -25,9 +26,15 @@ public interface MqttMessageDao {
     Long insertMqttMessage(MqttMessage m);
 
     @Query("SELECT name from codes where id = :code")
-    String getNameForCode(int code);
+    String getNameForCode(long code);
 
-    @Query("SELECT * FROM mqtt_messages WHERE push_server_id = :pushServerID and mqtt_accont_id = :accountID ORDER BY `when` DESC")
-    List<MqttMessage> getReceivedMessages(int pushServerID, int accountID);
+    @Query("SELECT * FROM codes")
+    List<Code> getCodes();
+
+    @Query("SELECT * FROM mqtt_messages, codes c1, codes c2 WHERE c2.name = :pushServer and c2.id = push_server_id and c1.name = :account and mqtt_accont_id = c1.id  ORDER BY `when` DESC")
+    public abstract DataSource.Factory<Integer, MqttMessage> getReceivedMessages(String pushServer, String account);
+
+    @Query("SELECT * FROM mqtt_messages ORDER BY `when` DESC")
+    public List<MqttMessage> loadReceivedMessages();
 
 }
