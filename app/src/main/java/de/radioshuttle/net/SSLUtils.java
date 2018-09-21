677be5ad02e44f11daee0211cc84c711a0b188fb
adalbert <>
2018-09-21 12:34:56 +0000

    Initial version. Helper to create socket factory with own trustore to accept all certificates. Used only in development.

---
+++ app/src/main/java/de/radioshuttle/net/SSLUtils.java
@@ -0,0 +1,43 @@
+/*
+ *	$Id$
+ *	This is an unpublished work copyright (c) 2018 HELIOS Software GmbH
+ *	30827 Garbsen, Germany.
+ */
+
+package de.radioshuttle.net;
+
+
+import java.security.SecureRandom;
+import java.security.cert.CertificateException;
+import java.security.cert.X509Certificate;
+
+import javax.net.ssl.SSLContext;
+import javax.net.ssl.SSLSocketFactory;
+import javax.net.ssl.TrustManager;
+import javax.net.ssl.X509TrustManager;
+
+public class SSLUtils {
+
+    public static SSLSocketFactory createSslSocketFactory() throws Exception {
+        SSLContext sslContext = SSLContext.getInstance("TLS");
+        sslContext.init(null, new TrustManager[] {new X509TrustManager() {
+
+            @Override
+            public void checkClientTrusted(X509Certificate[] chain, String authType) throws CertificateException {
+
+            }
+
+            @Override
+            public void checkServerTrusted(X509Certificate[] chain, String authType) throws CertificateException {
+
+            }
+
+            @Override
+            public X509Certificate[] getAcceptedIssuers() {
+                return new X509Certificate[0];
+            }
+        }}, new SecureRandom());
+        return sslContext.getSocketFactory();
+    }
+
+}
