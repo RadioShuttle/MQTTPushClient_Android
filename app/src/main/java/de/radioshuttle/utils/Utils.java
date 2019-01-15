1ebf1b2ab82b7d0a2345a055ae50d3191500fa25
adalbert <>
2019-01-15 14:32:26 +0000

    Helper functions from de.mqttpushclient.Utils copied to this class

--- app/src/main/java/de/radioshuttle/utils/Utils.java
+++ app/src/main/java/de/radioshuttle/utils/Utils.java
@@ -12,4 +12,17 @@ import java.util.concurrent.Executors;
 public class Utils {
     
     public static ExecutorService executor = Executors.newCachedThreadPool();
+
+    public static boolean isEmpty(String s) {
+        return s == null || s.trim().length() == 0;
+    }
+
+    public static boolean equals(Object a, Object b) {
+        return (a == b) || (a != null && a.equals(b));
+    }
+
+    public static boolean equals(String a, String b) {
+        return equals((Object) a, b) || (isEmpty(a) && isEmpty(b));
+    }
+
 }
