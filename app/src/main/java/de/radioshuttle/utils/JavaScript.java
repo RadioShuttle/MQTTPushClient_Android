df66ea649064bcd7bdf9e94f5fb8bdcd9945ff07
adalbert <>
2019-02-06 14:16:40 +0000

    java <-> javascript interface formatMsg() changed: parameter content replaced by payloadBase64 (payload base64 encoded) and payloadStr (payload interpreted as UTF8, replaced malformed and unmappable chars with the charsets default replacement). Javascript code: msg.content replaced by msg.text. Additional var "msg.raw" added (ArrayBuffer)

--- app/src/main/java/de/radioshuttle/utils/JavaScript.java
+++ app/src/main/java/de/radioshuttle/utils/JavaScript.java
@@ -6,6 +6,8 @@
 
 package de.radioshuttle.utils;
 
+import android.util.Base64;
+
 import com.squareup.duktape.Duktape;
 
 import de.radioshuttle.db.MqttMessage;
@@ -34,10 +36,14 @@ public class JavaScript {
     public String formatMsg(Context context, MqttMessage message, int notificationType) {
         String result = null;
         if (context instanceof DuktapeContext) {
+            String payloadBase64 = Base64.encodeToString(message.getPayload(), Base64.DEFAULT);
+            String payloadStr = new String(message.getPayload(), Utils.UTF_8);
+
             result = ((DuktapeContext) context).formatter.formatMsg(
                     String.valueOf(message.getWhen()),
                     message.getTopic(),
-                    message.getMsg(),
+                    payloadBase64,
+                    payloadStr,
                     notificationType
             );
         }
@@ -75,13 +81,17 @@ public class JavaScript {
 
             duktape.evaluate(""
                     + "var DuktapeMsgFormatter = { "
-                    + "  formatMsg: function(receivedDateMillis, topic, content, notificationType) { "
-                    + "  var msg = new Object(); "
-                    + "  if (!content) content = ''; "
+                    + "  formatMsg: function(receivedDateMillis, topic, payloadBase64, payloadStr, notificationType) { "
+                    + "  var msg = new Object();"
+                    + "  if (!payloadBase64) payloadBase64 = '';"
+                    + "  if (!payloadStr) payloadStr = '';"
                     + "  msg.receivedDate = new Date(Number(receivedDateMillis)); "
                     + "  msg.topic = topic; "
-                    + "  msg.content = content; "
+                    + "  msg.text = payloadStr; "
+                    + "  msg.content = payloadStr;" // deprecated, for backward comp
+                    + "  msg.raw = Duktape.dec('base64', payloadBase64);"
                     + "  msg.notificationType = notificationType; "
+                    + "  var content = payloadStr;"
                     + jsBody + "\n"
                     + "  return content;}"
                     + "};");
@@ -133,10 +143,7 @@ public class JavaScript {
 
 
     private interface DuktapeMsgFormatter {
-        String formatMsg(String receivedDateMillis, String topic, String content, int notificationType);
-    }
-
-    private JavaScript() {
+        String formatMsg(String receivedDateMillis, String topic, String payloadBase64, String payloadStr, int notificationType);
     }
 
     private static JavaScript js;
