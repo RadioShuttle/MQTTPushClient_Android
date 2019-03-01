c2ed9fc497bce0f6a34dc95a02a46d2a00fd3f68
adalbert <>
2019-03-01 10:36:28 +0000

    added member var deviceRemoved, which indicates if either removeDevice() or deleteInstannceID was succesful. deleteToken() will always be called, if set in request arguments. if deleteToken() was called and push server request failed, observer are notified after deleteToken() is finished (otherwise when the request to push server finished)

--- app/src/main/java/de/radioshuttle/net/DeleteToken.java
+++ app/src/main/java/de/radioshuttle/net/DeleteToken.java
@@ -27,20 +27,29 @@ public class DeleteToken extends Request {
     public DeleteToken(Context context, boolean deleteToken, PushAccount pushAccount, MutableLiveData<Request> accountLiveData) {
         super(context, pushAccount, accountLiveData);
         mDeleteToken = deleteToken;
+        deviceRemoved = false;
     }
 
     @Override
     public boolean perform() throws Exception {
         mConnection.removeDevice();
-
-
+        if (mConnection.lastReturnCode == Cmd.RC_OK) {
+            deviceRemoved = true;
+        }
         return true;
     }
 
     @Override
     protected void onPostExecute(PushAccount pushAccount) {
-        super.onPostExecute(pushAccount);
+        // Log.d(TAG, "onPostExecute: deleteToken: " + mDeleteToken + ", deviceRemoved: " + deviceRemoved);
+
+        if (!(mDeleteToken && !deviceRemoved)) {
+            /* do not call onPostExecute, if device was not removed and token shall be deleted in next op */
+            super.onPostExecute(pushAccount);
+        }
         if (mDeleteToken) {
+            // TODO: if delete account request failed, deletion of token will also fail: senderID and FirebaseApp data must be stored locally to use in DeleteToken.deleteToken() when push server is no available
+            mPushAccount.status = 1;
             Utils.executor.execute(new Runnable() {
                 @Override
                 public void run() {
@@ -52,38 +61,48 @@ public class DeleteToken extends Request {
 
     protected void deleteToken() {
         FirebaseApp app = null;
-        for (FirebaseApp a : FirebaseApp.getApps(mAppContext)) {
-            if (a.getName().equals(mSenderID)) {
-                app = FirebaseApp.getInstance(mSenderID);
-                FirebaseInstanceId id = FirebaseInstanceId.getInstance(app);
+        try {
+            for (FirebaseApp a : FirebaseApp.getApps(mAppContext)) {
+                if (a.getName().equals(mSenderID)) {
+                    app = FirebaseApp.getInstance(mSenderID);
+                    FirebaseInstanceId id = FirebaseInstanceId.getInstance(app);
 
-                Task<InstanceIdResult> t = id.getInstanceId();
-                try {
-                    Tasks.await(t);
-                } catch (Exception e) {
-                    Log.d(TAG, "Deletion of token for push server " +  mPushAccount.pushserver + "  failed: " + e.getMessage());
-                }
-
-                if (t.isSuccessful()) {
+                    Task<InstanceIdResult> t = id.getInstanceId();
                     try {
-                        id.deleteInstanceId();
-                        Log.d(TAG, "token deleted");
-                    } catch (IOException e) {
-                        Log.d(TAG, "deleteInstanceId() failed for " +  mPushAccount.pushserver + ": " + t.getResult().getToken());
-                        // throw new ClientError(e);
+                        Tasks.await(t);
+                    } catch (Exception e) {
+                        Log.d(TAG, "Deletion of token for push server " + mPushAccount.pushserver + "  failed: " + e.getMessage());
                     }
-                    Log.d(TAG, "Token deleted for " +  mPushAccount.pushserver + ": " + t.getResult().getToken());
-                } else {
-                    Log.d(TAG, "Deletion of token for push server " +  mPushAccount.pushserver + "  failed.");
-                    // throw new ClientError("Deletion of token failed.");
+
+                    if (t.isSuccessful()) {
+                        try {
+                            id.deleteInstanceId();
+                            deviceRemoved = true;
+                            Log.d(TAG, "token deleted");
+                        } catch (IOException e) {
+                            Log.d(TAG, "deleteInstanceId() failed for " + mPushAccount.pushserver + ": " + t.getResult().getToken());
+                            // throw new ClientError(e);
+                        }
+                        Log.d(TAG, "Token deleted for " + mPushAccount.pushserver + ": " + t.getResult().getToken());
+                    } else {
+                        Log.d(TAG, "Deletion of token for push server " + mPushAccount.pushserver + "  failed.");
+                        // throw new ClientError("Deletion of token failed.");
+                    }
+                    break;
                 }
-                break;
+            }
+        } catch (Exception e) {
+            Log.d(TAG, "Deletion of token failed: " + e.getMessage());
+        } finally {
+            mPushAccount.status = 0;
+            if (mAccountLiveData != null) {
+                mAccountLiveData.postValue(this);
             }
         }
-
     }
 
-
+    /** set if eiter removeDevice() request was succesfull or deleteInstanceID*/
+    public boolean deviceRemoved;
     private boolean mDeleteToken;
 
     private final static String TAG = DeleteToken.class.getSimpleName();
