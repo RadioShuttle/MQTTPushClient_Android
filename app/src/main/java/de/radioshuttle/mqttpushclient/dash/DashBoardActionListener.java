0d36defb53c0295d9b0b6398fdb984352d668083
adalbert <>
2019-03-07 11:09:04 +0000

    Initial version.

---
+++ app/src/main/java/de/radioshuttle/mqttpushclient/dash/DashBoardActionListener.java
@@ -0,0 +1,11 @@
+/*
+ *	$Id$
+ *	This is an unpublished work copyright (c) 2019 HELIOS Software GmbH
+ *	30827 Garbsen, Germany.
+ */
+
+package de.radioshuttle.mqttpushclient.dash;
+
+public interface DashBoardActionListener {
+    void onItemClicked(Item item);
+}
