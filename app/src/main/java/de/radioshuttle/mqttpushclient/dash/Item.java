19df2a59d4c529bf2d15ef60d240785853f82195
adalbert <>
2019-03-13 10:13:05 +0000

    removed old properties, added helper to convert json to dashboard objects

--- app/src/main/java/de/radioshuttle/mqttpushclient/dash/Item.java
+++ app/src/main/java/de/radioshuttle/mqttpushclient/dash/Item.java
@@ -7,44 +7,140 @@
 package de.radioshuttle.mqttpushclient.dash;
 
 
+import android.util.Log;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.LinkedList;
+import java.util.Map;
+
 public abstract class Item {
     public Item() {
         id = cnt++;
     }
 
-    public int id; // transient (for internal use)
+    public int id; // transient (unique id for internal use)
 
-    public int groupIdx;
-    public int orderInGroup;
     public String label;
 
-    public abstract int getType();
+    public JSONObject toJSONObject() throws JSONException {
+        JSONObject o = new JSONObject();
+        if (!(this instanceof GroupItem)) {
+            o.put("type", getType());
+        }
+        o.put("label", label);
+        return o;
+    }
 
-    public final static int TYPE_GROUP = 0;
-    public final static int TYPE_TEXT = 1;
+    protected void setJSONData(JSONObject o) {
+        label = o.optString("label");
+    }
 
-    public static class Comparator implements java.util.Comparator<Item> {
+    public static Item createItemFromJSONObject(JSONObject o) {
+        Item item = null;
+        String type = o.optString("type");
+        switch (type) {
+            case "text": item = new TextItem(); break;
+        }
+        if (item != null) {
+            item.setJSONData(o);
+        }
+        return item;
+    }
 
-        @Override
-        public int compare(Item o1, Item o2) {
-            int cmp = 0;
-            int g1 = o1 == null ? 0 : o1.groupIdx;
-            int g2 = o2 == null ? 0 : o2.groupIdx;
-            int s1 = o1 == null ? 0 : o1.orderInGroup;
-            int s2 = o2 == null ? 0 : o2.orderInGroup;
-            if (g1 < g2) {
-                cmp = -1;
-            } else if (g1 > g2) {
-                cmp = 1;
-            } else {
-                if (s1 < s2) {
-                    cmp = -1;
-                } else if (s1 > s2) {
-                    cmp = 1;
+    public static void createItemsFromJSONString(String json, LinkedList<GroupItem> groups, HashMap<Integer, LinkedList<Item>> groupItems) {
+        if (!de.radioshuttle.utils.Utils.isEmpty(json) && groups != null && groupItems != null) {
+            try {
+                JSONArray groupArray = new JSONArray(json);
+                GroupItem groupItem;
+                Item item;
+                JSONObject groupJSON, itemJSON;
+                for (int i = 0; i < groupArray.length(); i++) {
+                    groupItem = new GroupItem();
+                    groupJSON = groupArray.getJSONObject(i);
+                    if (groupJSON.optInt("id", -1) != -1) {
+                        groupItem.id = groupJSON.optInt("id");
+                        if (groupItem.id >= cnt) {
+                            cnt = groupItem.id + 1;
+                        }
+                    }
+                    groupItem.setJSONData(groupJSON);
+                    JSONArray itemArray = groupJSON.getJSONArray("items");
+                    groups.add(groupItem);
+                    LinkedList<Item> items = new LinkedList<>();
+                    groupItems.put(groupItem.id, items);
+
+                    for (int j = 0; j < itemArray.length(); j++) {
+                        itemJSON = itemArray.getJSONObject(j);
+                        item = createItemFromJSONObject(itemJSON);
+                        if (itemJSON.optInt("id", -1) != -1) {
+                            item.id = itemJSON.optInt("id");
+                            if (item.id >= cnt) {
+                                cnt = item.id  + 1;
+                            }
+                        }
+                        items.add(item);
+                    }
                 }
+            } catch (JSONException e) {
+                Log.d(TAG, "Error parsing JSON: " + e.getMessage());
             }
-            return cmp;
         }
     }
+
+    public static JSONArray createJSONStrFromItems(LinkedList<GroupItem> groups, HashMap<Integer, LinkedList<Item>> groupItems, boolean storeGroupID) {
+        JSONArray groupItemArray = new JSONArray();
+        if (groups != null && groupItems != null) {
+            JSONObject groupJSON = null;
+            JSONObject itemJSON = null;
+            JSONArray itemArray = null;
+
+            Iterator<GroupItem> it = groups.iterator();
+            LinkedList<Item> items;
+            while(it.hasNext()) {
+                GroupItem g = it.next();
+                try {
+                    groupJSON = g.toJSONObject();
+                    if (storeGroupID) {
+                        groupJSON.put("id", g.id);
+                    }
+                    itemArray = new JSONArray();
+                    groupJSON.put("items", itemArray);
+                    groupItemArray.put(groupJSON);
+                } catch(JSONException e) {
+                    Log.d(TAG, "Error createJSONStrFromItems: " + e.getMessage());
+                    continue;
+                }
+                items = groupItems.get(g.id);
+                if (items != null) {
+                    Iterator<Item> it2 = items.iterator();
+                    while(it2.hasNext()) {
+                        Item e = it2.next();
+                        try {
+                            itemJSON = e.toJSONObject();
+                            if (storeGroupID) {
+                                itemJSON.put("id", e.id);
+                            }
+                        } catch(JSONException e2) {
+                            Log.d(TAG, "Error createJSONStrFromItems: " + e2.getMessage());
+                            continue;
+                        }
+                        itemArray.put(itemJSON);
+                    }
+                }
+
+            }
+        }
+        return groupItemArray;
+    }
+
+    public abstract String getType();
+
     private static int cnt = 0;
+
+    protected static String TAG = Item.class.getSimpleName();
 }
