9271d5bd8506ff9b59cd54cde5a8c3ab94b7676a
adalbert <>
2019-04-01 12:58:21 +0000

    added JSONObject to hold additional data (message content, overwritten item settings,...). Some helpers have been moved to DBUtils

--- app/src/main/java/de/radioshuttle/mqttpushclient/dash/Item.java
+++ app/src/main/java/de/radioshuttle/mqttpushclient/dash/Item.java
@@ -7,6 +7,7 @@
 package de.radioshuttle.mqttpushclient.dash;
 
 
+import android.util.JsonReader;
 import android.util.Log;
 
 import org.json.JSONArray;
@@ -23,6 +24,7 @@ import de.radioshuttle.utils.Utils;
 public abstract class Item {
     public Item() {
         id = cnt++;
+        uiProperties = new JSONObject();
     }
 
     public int id; // transient (unique id for internal use)
@@ -31,6 +33,7 @@ public abstract class Item {
     public int background;
     public int textsize; // 0 - default, 1 - small, 2 - medium, 3 - large
     public String topic_s;
+    public String script_f;
 
     public String label;
 
@@ -44,6 +47,7 @@ public abstract class Item {
         o.put("color", color);
         o.put("background", background);
         o.put("textsize", textsize);
+        o.put("script_f", (script_f == null ? "" : script_f));
 
         return o;
     }
@@ -54,6 +58,7 @@ public abstract class Item {
         color = o.optInt("color");
         textsize = o.optInt("textsize");
         topic_s = o.optString("topic_s");
+        script_f = o.optString("script_f");
     }
 
     public static Item createItemFromJSONObject(JSONObject o) {
@@ -68,95 +73,11 @@ public abstract class Item {
         return item;
     }
 
-    public static void createItemsFromJSONString(String json, LinkedList<GroupItem> groups, HashMap<Integer, LinkedList<Item>> groupItems) {
-        if (!Utils.isEmpty(json) && groups != null && groupItems != null) {
-            try {
-                JSONArray groupArray = new JSONArray(json);
-                GroupItem groupItem;
-                Item item;
-                JSONObject groupJSON, itemJSON;
-                for (int i = 0; i < groupArray.length(); i++) {
-                    groupItem = new GroupItem();
-                    groupJSON = groupArray.getJSONObject(i);
-                    if (groupJSON.optInt("id", -1) != -1) {
-                        groupItem.id = groupJSON.optInt("id");
-                        if (groupItem.id >= cnt) {
-                            cnt = groupItem.id + 1;
-                        }
-                    }
-                    groupItem.setJSONData(groupJSON);
-                    JSONArray itemArray = groupJSON.getJSONArray("items");
-                    groups.add(groupItem);
-                    LinkedList<Item> items = new LinkedList<>();
-                    groupItems.put(groupItem.id, items);
-
-                    for (int j = 0; j < itemArray.length(); j++) {
-                        itemJSON = itemArray.getJSONObject(j);
-                        item = createItemFromJSONObject(itemJSON);
-                        if (itemJSON.optInt("id", -1) != -1) {
-                            item.id = itemJSON.optInt("id");
-                            if (item.id >= cnt) {
-                                cnt = item.id  + 1;
-                            }
-                        }
-                        items.add(item);
-                    }
-                }
-            } catch (JSONException e) {
-                Log.d(TAG, "Error parsing JSON: " + e.getMessage());
-            }
-        }
-    }
-
-    public static JSONArray createJSONStrFromItems(LinkedList<GroupItem> groups, HashMap<Integer, LinkedList<Item>> groupItems, boolean storeGroupID) {
-        JSONArray groupItemArray = new JSONArray();
-        if (groups != null && groupItems != null) {
-            JSONObject groupJSON = null;
-            JSONObject itemJSON = null;
-            JSONArray itemArray = null;
-
-            Iterator<GroupItem> it = groups.iterator();
-            LinkedList<Item> items;
-            while(it.hasNext()) {
-                GroupItem g = it.next();
-                try {
-                    groupJSON = g.toJSONObject();
-                    if (storeGroupID) {
-                        groupJSON.put("id", g.id);
-                    }
-                    itemArray = new JSONArray();
-                    groupJSON.put("items", itemArray);
-                    groupItemArray.put(groupJSON);
-                } catch(JSONException e) {
-                    Log.d(TAG, "Error createJSONStrFromItems: " + e.getMessage());
-                    continue;
-                }
-                items = groupItems.get(g.id);
-                if (items != null) {
-                    Iterator<Item> it2 = items.iterator();
-                    while(it2.hasNext()) {
-                        Item e = it2.next();
-                        try {
-                            itemJSON = e.toJSONObject();
-                            if (storeGroupID) {
-                                itemJSON.put("id", e.id);
-                            }
-                        } catch(JSONException e2) {
-                            Log.d(TAG, "Error createJSONStrFromItems: " + e2.getMessage());
-                            continue;
-                        }
-                        itemArray.put(itemJSON);
-                    }
-                }
-
-            }
-        }
-        return groupItemArray;
-    }
+    public JSONObject uiProperties;
 
     public abstract String getType();
 
-    private static int cnt = 0;
+    protected static int cnt = 0;
 
     public final static int TEXTSIZE_SMALL = 1;
     public final static int TEXTSIZE_MEDIUM = 2;
