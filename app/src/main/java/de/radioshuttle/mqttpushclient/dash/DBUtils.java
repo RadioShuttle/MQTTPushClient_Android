a8d068b04ca10fb970a4e8ea3e9c002ae316e5c9
adalbert <>
2019-04-01 13:17:37 +0000

    added test data generator thread, moved Item.createItemsFromJSONString(), Item.createJSONStrFromItems() to this class

--- app/src/main/java/de/radioshuttle/mqttpushclient/dash/DBUtils.java
+++ app/src/main/java/de/radioshuttle/mqttpushclient/dash/DBUtils.java
@@ -10,18 +10,27 @@ import android.app.AlertDialog;
 import android.content.Context;
 import android.content.DialogInterface;
 import android.graphics.Rect;
+import android.os.Handler;
+import android.os.Looper;
+import android.util.Log;
 import android.view.View;
 
-import java.util.Calendar;
-import java.util.GregorianCalendar;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.LinkedList;
 import java.util.List;
+import java.util.Random;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.GridLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
-import de.radioshuttle.fcm.Notifications;
-import de.radioshuttle.mqttpushclient.MessagesPagedListAdapter;
+import de.radioshuttle.db.MqttMessage;
 import de.radioshuttle.mqttpushclient.R;
+import de.radioshuttle.utils.Utils;
 
 public class DBUtils {
 
@@ -107,4 +116,119 @@ public class DBUtils {
         AlertDialog dlg = builder.create();
         dlg.show();
     }
+
+    public static void createItemsFromJSONString(String json, LinkedList<GroupItem> groups, HashMap<Integer, LinkedList<Item>> groupItems) {
+        if (!Utils.isEmpty(json) && groups != null && groupItems != null) {
+            try {
+                JSONArray groupArray = new JSONArray(json);
+                GroupItem groupItem;
+                Item item;
+                JSONObject groupJSON, itemJSON;
+                for (int i = 0; i < groupArray.length(); i++) {
+                    groupItem = new GroupItem();
+                    groupJSON = groupArray.getJSONObject(i);
+                    if (groupJSON.optInt("id", -1) != -1) {
+                        groupItem.id = groupJSON.optInt("id");
+                        if (groupItem.id >= Item.cnt) {
+                            Item.cnt = groupItem.id + 1;
+                        }
+                    }
+                    groupItem.setJSONData(groupJSON);
+                    JSONArray itemArray = groupJSON.getJSONArray("items");
+                    groups.add(groupItem);
+                    LinkedList<Item> items = new LinkedList<>();
+                    groupItems.put(groupItem.id, items);
+
+                    for (int j = 0; j < itemArray.length(); j++) {
+                        itemJSON = itemArray.getJSONObject(j);
+                        item = Item.createItemFromJSONObject(itemJSON);
+                        if (itemJSON.optInt("id", -1) != -1) {
+                            item.id = itemJSON.optInt("id");
+                            if (item.id >= Item.cnt) {
+                                Item.cnt = item.id  + 1;
+                            }
+                        }
+                        items.add(item);
+                    }
+                }
+            } catch (JSONException e) {
+                Log.d(Item.TAG, "Error parsing JSON: " + e.getMessage());
+            }
+        }
+    }
+
+    public static JSONArray createJSONStrFromItems(LinkedList<GroupItem> groups, HashMap<Integer, LinkedList<Item>> groupItems, boolean storeGroupID) {
+        JSONArray groupItemArray = new JSONArray();
+        if (groups != null && groupItems != null) {
+            JSONObject groupJSON = null;
+            JSONObject itemJSON = null;
+            JSONArray itemArray = null;
+
+            Iterator<GroupItem> it = groups.iterator();
+            LinkedList<Item> items;
+            while(it.hasNext()) {
+                GroupItem g = it.next();
+                try {
+                    groupJSON = g.toJSONObject();
+                    if (storeGroupID) {
+                        groupJSON.put("id", g.id);
+                    }
+                    itemArray = new JSONArray();
+                    groupJSON.put("items", itemArray);
+                    groupItemArray.put(groupJSON);
+                } catch(JSONException e) {
+                    Log.d(Item.TAG, "Error createJSONStrFromItems: " + e.getMessage());
+                    continue;
+                }
+                items = groupItems.get(g.id);
+                if (items != null) {
+                    Iterator<Item> it2 = items.iterator();
+                    while(it2.hasNext()) {
+                        Item e = it2.next();
+                        try {
+                            itemJSON = e.toJSONObject();
+                            if (storeGroupID) {
+                                itemJSON.put("id", e.id);
+                            }
+                        } catch(JSONException e2) {
+                            Log.d(Item.TAG, "Error createJSONStrFromItems: " + e2.getMessage());
+                            continue;
+                        }
+                        itemArray.put(itemJSON);
+                    }
+                }
+
+            }
+        }
+        return groupItemArray;
+    }
+
+    public static Thread testDataThread(final DashBoardViewModel vm) {
+        return new Thread(new Runnable() {
+            @Override
+            public void run() {
+                Handler uiHandler = new Handler(Looper.getMainLooper());
+                final Random random = new Random();
+
+                while(!Thread.interrupted()) {
+                    try {
+                        Thread.sleep(5000L);
+                        uiHandler.post(new Runnable() {
+                            @Override
+                            public void run() {
+                                MqttMessage m = new MqttMessage();
+                                m.setWhen(System.currentTimeMillis());
+                                m.setTopic("test");
+                                m.setPayload(String.valueOf(15 + random.nextInt(15)).getBytes());
+                                vm.onMessageReceived(m);
+                            }
+                        });
+                    } catch (InterruptedException e) {
+                        Thread.currentThread().interrupt();
+                    }
+                }
+                Log.d(DashBoardViewModel.TAG, "test data thread terminated");
+            }
+        });
+    };
 }
