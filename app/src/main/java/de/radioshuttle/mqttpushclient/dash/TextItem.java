abb003059e4dd2e460240616e2650c06459aa4b9
adalbert <>
2019-06-27 13:34:54 +0000

    new member input type

--- app/src/main/java/de/radioshuttle/mqttpushclient/dash/TextItem.java
+++ app/src/main/java/de/radioshuttle/mqttpushclient/dash/TextItem.java
@@ -6,11 +6,29 @@
 
 package de.radioshuttle.mqttpushclient.dash;
 
+import org.json.JSONException;
+import org.json.JSONObject;
+
 public class TextItem extends Item {
 
     @Override
     public String getType() {
         return "text";
     }
+
+    @Override
+    public JSONObject toJSONObject() throws JSONException {
+        JSONObject o = super.toJSONObject();
+        o.put("input_type", inputtype);
+        return o;
+    }
+
+    protected void setJSONData(JSONObject o) {
+        super.setJSONData(o);
+        inputtype = o.optInt("input_type");
+    }
+
+
+    public int inputtype;
 
 }
