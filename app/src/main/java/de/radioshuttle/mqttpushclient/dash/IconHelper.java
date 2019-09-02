fc268e7cf70e317aa8d1dbc38726de1f0e4fea7b
adalbert <>
2019-09-02 14:59:39 +0000

    new images

--- app/src/main/java/de/radioshuttle/mqttpushclient/dash/IconHelper.java
+++ app/src/main/java/de/radioshuttle/mqttpushclient/dash/IconHelper.java
@@ -19,13 +19,24 @@ public class IconHelper {
     static {
         INTENRAL_ICONS = new LinkedHashMap<>();
 
+        // toggle
+        INTENRAL_ICONS.put("res://internal/toggle_on", R.drawable.xic_toggle_on);
+        INTENRAL_ICONS.put("res://internal/toggle_off", R.drawable.xic_toggle_off);
+        INTENRAL_ICONS.put("res://internal/check_box", R.drawable.xic_check_box);
+        INTENRAL_ICONS.put("res://internal/check_box_outline_blank", R.drawable.xic_check_box_outline_blank);
+        INTENRAL_ICONS.put("res://internal/indeterminate_check_box", R.drawable.xic_indeterminate_check_box);
 
+        INTENRAL_ICONS.put("res://internal/radio_button_checked", R.drawable.xic_radio_button_checked);
+        INTENRAL_ICONS.put("res://internal/radio_button_unchecked", R.drawable.xic_radio_button_unchecked);
+
+        // alarm
         INTENRAL_ICONS.put("res://internal/notifications", R.drawable.xic_notifications);
         INTENRAL_ICONS.put("res://internal/notifications_active", R.drawable.xic_notifications_active);
         INTENRAL_ICONS.put("res://internal/notifications_none", R.drawable.xic_notifications_none);
         INTENRAL_ICONS.put("res://internal/notifications_off", R.drawable.xic_notifications_off);
         INTENRAL_ICONS.put("res://internal/notifications_paused", R.drawable.xic_notifications_paused);
 
+        // msic
         INTENRAL_ICONS.put("res://internal/emoji_objects", R.drawable.xic_emoji_objects);
 
         INTENRAL_ICONS.put("res://internal/sentiment_dissatisfied", R.drawable.xic_sentiment_dissatisfied);
