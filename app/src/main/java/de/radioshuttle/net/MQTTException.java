7e3d2843597c4313f26965ad36da293ea338e4e3
adalbert <>
2019-09-10 10:29:16 +0000

    added constructor with account info parameter

--- app/src/main/java/de/radioshuttle/net/MQTTException.java
+++ app/src/main/java/de/radioshuttle/net/MQTTException.java
@@ -11,4 +11,12 @@ public class MQTTException extends ServerError {
     public MQTTException(int code, String msg) {
         super(code, msg);
     }
+
+    public MQTTException(int code, String msg, int loginAccountInfo) {
+        super(code, msg);
+        accountInfo = loginAccountInfo;
+    }
+
+    /** 1 = MQTT error performing login, but accout data matches local stored credntials */
+    public int accountInfo; // set if MQTT exception occured on server while login
 }
