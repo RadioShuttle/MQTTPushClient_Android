49868b9d4263433c4bce111bc04d4873f7aa9ffd
adalbert <>
2019-09-11 11:40:56 +0000

    create LiveData object (paged list for internal images)

--- app/src/main/java/de/radioshuttle/mqttpushclient/dash/ImageChooserViewModel.java
+++ app/src/main/java/de/radioshuttle/mqttpushclient/dash/ImageChooserViewModel.java
@@ -1,17 +1,31 @@
+/*
+ *	$Id$
+ *	This is an unpublished work copyright (c) 2019 HELIOS Software GmbH
+ *	30827 Garbsen, Germany.
+ */
+
 package de.radioshuttle.mqttpushclient.dash;
 
 import android.app.Application;
 
 import androidx.annotation.NonNull;
 import androidx.lifecycle.AndroidViewModel;
+import androidx.lifecycle.LiveData;
 import androidx.lifecycle.ViewModel;
 import androidx.lifecycle.ViewModelProvider;
+import androidx.paging.LivePagedListBuilder;
+import androidx.paging.PagedList;
 
 public class ImageChooserViewModel extends AndroidViewModel {
 
     public ImageChooserViewModel(@NonNull Application application) {
         super(application);
+
+        ImageDataSource.Factory factory = new ImageDataSource.Factory(this);
+        mLiveDataInternalImages = new LivePagedListBuilder(factory, 100).build();
     }
+
+    public final LiveData<PagedList<ImageResource>> mLiveDataInternalImages;
 
     public static class Factory implements ViewModelProvider.Factory {
 
@@ -27,6 +41,5 @@ public class ImageChooserViewModel extends AndroidViewModel {
 
         Application app;
     }
-
 
 }
