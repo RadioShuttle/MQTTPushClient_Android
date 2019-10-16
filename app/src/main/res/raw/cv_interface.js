62b65c9edf259ee7fe2f28cfb21bc994e028ae91
adalbert <>
2019-10-16 07:55:48 +0000

    added javascript error handler for uncaught exceptions.

--- app/src/main/res/raw/cv_interface.js
+++ app/src/main/res/raw/cv_interface.js
@@ -23,4 +23,24 @@ function _onUpdate(receivedDateMillis, topic, payloadStr, payloadBase64) {
     onUpdate(msg);
 }
 
+function _onErrorDashboardScript(e) {
+  var string = e.message.toLowerCase();
+  var substring = "script error";
+  if (string.indexOf(substring) > -1){
+    /* for security reasons there are no detail infos available for external scripts */
+    PushApp.log('Script Error (extenal script). No details available.'); /* TODO: call view.setError() if implemented */
+  } else {
+    var message = [
+      'Message: ' + e.message,
+      /* 'URL: ' + url, */
+      'Line: ' + e.lineno,
+      'Column: ' + e.colno,
+      'Error object: ' + JSON.stringify(e.error)
+    ].join(' - ');
+    PushApp.log("Script Error: " + message); /* TODO: call view.setError() if implemented */
+  }
+  return false;
+};
+window.addEventListener('error', _onErrorDashboardScript);
+
 /* TODO: https://developer.mozilla.org/en-US/docs/Web/API/WindowBase64/Base64_encoding_and_decoding */
