e4fad26db479bbd2ae443551bacd718c87230af2
adalbert <>
2019-10-18 14:27:01 +0000

    removed js error handler (replaced by webChromeClient callback)

--- app/src/main/res/raw/cv_interface.js
+++ app/src/main/res/raw/cv_interface.js
@@ -23,24 +23,4 @@ function _onMqttMessage(receivedDateMillis, topic, payloadStr, payloadBase64) {
     onMqttMessage(msg);
 }
 
-function _onDashboardScriptError(e) {
-  var string = e.message.toLowerCase();
-  var substring = "script error";
-  if (string.indexOf(substring) > -1){
-    /* for security reasons there are no detail infos available for external scripts */
-    MqttPushClient.log('Script Error (extenal script). No details available.'); /* TODO: call view.setError() if implemented */
-  } else {
-    var message = [
-      'Message: ' + e.message,
-      /* 'URL: ' + url, */
-      'Line: ' + e.lineno,
-      'Column: ' + e.colno,
-      'Error object: ' + JSON.stringify(e.error)
-    ].join(' - ');
-    MqttPushClient.log("Script Error: " + message); /* TODO: call view.setError() if implemented */
-  }
-  return false;
-};
-window.addEventListener('error', _onDashboardScriptError);
-
 /* TODO: https://developer.mozilla.org/en-US/docs/Web/API/WindowBase64/Base64_encoding_and_decoding */
