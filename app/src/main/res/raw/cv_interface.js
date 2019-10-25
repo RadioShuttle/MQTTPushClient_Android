d4abd215caa97725d96c2d9e62e8a16998888a6c
adalbert <>
2019-10-25 14:47:51 +0000

    publish() moved to MQTT.publish. Implemented calls to MQTT._publish(). Added helper to convert ArrayBuffer objects to hexStr

--- app/src/main/res/raw/cv_interface.js
+++ app/src/main/res/raw/cv_interface.js
@@ -1,16 +1,23 @@
-function publish(topic, msg, retain) {
-    var buf = null;
-
-    if (typeof msg === 'string') {
-        MqttPushClient.publish(topic, msg, retain);
-    } else if (msg instanceof ArrayBuffer) {
-        /* TODO */
-        /* _publishBase64(); */
-        throw "publish(): arg msg of type ArrayBuffer not supported yet";
-    } else {
-        throw "publish(): arg msg must be of type String or ArrayBuffer";
-    }
+function buf2hex(buffer) {
+  var byteArray = new Uint8Array(buffer);
+  var hexStr = '';
+  for(let i = 0; i < byteArray.length; i++) {
+    var hex = byteArray[i].toString(16);
+    var paddedHex = ('00' + hex).slice(-2);
+    hexStr += paddedHex;
+  }
+  return hexStr;
 }
+
+MQTT.publish = function (topic, msg, retain) {
+    if (typeof msg === 'string') {
+        MQTT._publishStr(topic, msg, retain === true);
+    } else if (msg instanceof ArrayBuffer) {
+        MQTT._publishHex(topic, buf2hex(msg), retain === true);
+    } else {
+        throw "MQTT.publish(): arg msg must be of type String or ArrayBuffer";
+    }
+};
 
 function _onMqttMessage(receivedDateMillis, topic, payloadStr, payloadBase64) {
     var msg = new Object();
@@ -19,8 +26,6 @@ function _onMqttMessage(receivedDateMillis, topic, payloadStr, payloadBase64) {
     msg.receivedDate = new Date(Number(receivedDateMillis));
     msg.topic = topic;
     msg.text = payloadStr;
-    /* msg.raw = _base64DecToArr(payloadBase64); */ /* TODO */
+    /* msg.raw = _base64DecToArr(payloadBase64); */ /* TODO consider using hex insread of base64 */
     onMqttMessage(msg);
 }
-
-/* TODO: https://developer.mozilla.org/en-US/docs/Web/API/WindowBase64/Base64_encoding_and_decoding */
