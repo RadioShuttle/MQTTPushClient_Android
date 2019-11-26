0adaf72576c33221fb2bfa3bc9169ab74641a83b
adalbert <>
2019-11-26 14:07:00 +0000

    added header

--- app/src/main/java/de/radioshuttle/mqttpushclient/dash/OptionListAdapter.java
+++ app/src/main/java/de/radioshuttle/mqttpushclient/dash/OptionListAdapter.java
@@ -1,3 +1,9 @@
+/*
+ * $Id$
+ * This is an unpublished work copyright (c) 2018 HELIOS Software GmbH
+ * 30827 Garbsen, Germany
+ */
+
 package de.radioshuttle.mqttpushclient.dash;
 
 import android.content.Context;
