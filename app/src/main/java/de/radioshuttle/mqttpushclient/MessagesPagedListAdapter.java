28fbafc75f86206c63229468adf543d457068443
adalbert-hel <32635945+adalbert-hel@users.noreply.github.com>
2020-04-27 16:49:26 +0200

    tint background with message.background and textcolor if set

--- app/src/main/java/de/radioshuttle/mqttpushclient/MessagesPagedListAdapter.java
+++ app/src/main/java/de/radioshuttle/mqttpushclient/MessagesPagedListAdapter.java
@@ -6,6 +6,8 @@
 
 package de.radioshuttle.mqttpushclient;
 
+import androidx.constraintlayout.widget.ConstraintLayout;
+import androidx.core.graphics.drawable.DrawableCompat;
 import androidx.paging.PagedList;
 import androidx.paging.PagedListAdapter;
 
@@ -15,7 +17,11 @@ import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.DiffUtil;
 import androidx.recyclerview.widget.RecyclerView;
+
+import android.graphics.drawable.Drawable;
+import android.os.Build;
 import android.text.format.DateUtils;
+import android.util.DisplayMetrics;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -27,6 +33,7 @@ import java.util.HashSet;
 import java.util.Locale;
 
 import de.radioshuttle.db.MqttMessage;
+import de.radioshuttle.mqttpushclient.dash.DColor;
 import de.radioshuttle.utils.Utils;
 
 public class MessagesPagedListAdapter extends PagedListAdapter<MqttMessage, MessagesPagedListAdapter.ViewHolder>{
@@ -40,6 +47,8 @@ public class MessagesPagedListAdapter extends PagedListAdapter<MqttMessage, Mess
                 DateFormat.SHORT,
                 Locale.getDefault());
         timeFormatter = DateFormat.getTimeInstance(DateFormat.SHORT);
+        mDisplayMetrics = activity.getResources().getDisplayMetrics();
+
     }
 
     @NonNull
@@ -50,13 +59,24 @@ public class MessagesPagedListAdapter extends PagedListAdapter<MqttMessage, Mess
 
         holder.subject = view.findViewById(R.id.subject);
         holder.msg = view.findViewById(R.id.msg);
+        holder.backgroundLayer = view.findViewById(R.id.backgroundLayer);
 
+        holder.subject.getTextColors().getDefaultColor();
+
+        if (mDefaultTextColorBackground == null) {
+            mDefaultTextColorSubject = holder.subject.getCurrentTextColor();
+            mDefaultTextColorMsg = holder.msg.getCurrentTextColor();
+            mDefaultTextColorBackground = 0; // assume transparent color
+        }
         return holder;
     }
 
     @Override
     public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
         MqttMessage m = getItem(position);
+
+        int expBackground = mDefaultTextColorBackground;
+        int expForegroundMsg = mDefaultTextColorMsg;
 
         if (m != null) {
             StringBuilder sb = new StringBuilder();
@@ -72,11 +92,42 @@ public class MessagesPagedListAdapter extends PagedListAdapter<MqttMessage, Mess
             holder.msg.setText(new String(m.getPayload(), Utils.UTF_8));
 
             holder.itemView.setSelected(selectedItems.contains(m.getId()));
+
+            if (m.backgroundColor != null && m.backgroundColor != DColor.CLEAR && m.backgroundColor != DColor.OS_DEFAULT) {
+                expBackground = m.backgroundColor.intValue();
+            }
+            if (m.textColor != null && m.textColor != DColor.CLEAR && m.textColor != DColor.OS_DEFAULT) {
+                expForegroundMsg = m.textColor.intValue();
+            }
+
         } else {
             holder.subject.setText(null);
             holder.msg.setText(null);
-            if (holder.itemView.isSelected())
+            if (holder.itemView.isSelected()) {
                 holder.itemView.setSelected(false);
+            }
+        }
+
+        if (expForegroundMsg != holder.currentForegroundMsg) {
+            holder.msg.setTextColor(expForegroundMsg);
+            holder.currentForegroundMsg = expForegroundMsg;
+        }
+        if (expBackground != holder.currentBackground) {
+            if (holder.backgroundLayer.getBackground() != null) {
+                Drawable d = DrawableCompat.wrap(holder.backgroundLayer.getBackground());
+                holder.backgroundLayer.setBackground(d);
+                DrawableCompat.setTint(holder.backgroundLayer.getBackground(), expBackground);
+                holder.currentBackground = expBackground;
+            }
+        }
+        if (holder.currentBackground != mDefaultTextColorBackground) {
+            if (holder.backgroundLayer.getVisibility() != View.VISIBLE) {
+                holder.backgroundLayer.setVisibility(View.VISIBLE);
+            }
+        } else {
+            if (holder.backgroundLayer.getVisibility() != View.GONE) {
+                holder.backgroundLayer.setVisibility(View.GONE);
+            }
         }
     }
 
@@ -125,10 +176,20 @@ public class MessagesPagedListAdapter extends PagedListAdapter<MqttMessage, Mess
             super(itemView);
         }
 
+        View backgroundLayer;
         TextView subject;
         TextView msg;
 
+        int currentBackground;
+        int currentForegroundSubject;
+        int currentForegroundMsg;
+        boolean selected;
     }
+
+    Integer mDefaultTextColorSubject;
+    Integer mDefaultTextColorMsg;
+    Integer mDefaultTextColorBackground;
+    DisplayMetrics mDisplayMetrics;
 
     private final static String TAG = MessagesPagedListAdapter.class.getSimpleName();
 }
